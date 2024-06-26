61284dbd57809aa7800c17e6fe1078d1f06bcd42
CiceroNila <ciceronila@yahoo.com>
2024-06-26 20:41:27 +0300

    added hello world program example 26.06.2024

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,9 @@
 //Deocamdata mesaj - Learning VSC
+//Denumirea clasei este bine sa fie aceeasi cu numele fisierului 26.06.2024
+public class HelloWorld {
+
+	public static void main (String[] args){
+		System.out.println("Hello World! We are learning VSC.");	
+	}
+} 
+
